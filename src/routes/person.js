ee77504e3ee5384411c850b311a420bd450fbdbf
somya <somya.singhal0797@gmail.com>
2019-06-21 17:02:05 +0530

    express get request

---
+++ src/routes/person.js
@@ -0,0 +1,11 @@
+let express = require('express')
+
+let router = express.Router()
+
+router.get('/person', (req,res) => {
+    res.send('You have requested a person')
+})
+
+
+
+module.exports = router
