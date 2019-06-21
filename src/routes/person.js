be9a5806666a33c499f261697aca5f51b046e0d2
somya <somya.singhal0797@gmail.com>
2019-06-21 18:39:10 +0530

    query string

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,14 +1,20 @@
 let express = require('express')
 
 let router = express.Router()
-
 router.get('/person', (req,res) => {
+if(req.query.name){
+    res.send(`You have requested a person ${req.query.name}`)
+}
+else{
     res.send('You have requested a person')
+}
+
+
 })
 //Param property on the request obj
-router.get('/person/:name', (req,res) => {
-    res.send(`You have requested a person ${req.params.name}`)
-})
+//router.get('/person/:name', (req,res) => {
+  //  res.send(`You have requested a person ${req.params.name}`)
+//})
 
 
 module.exports = router
