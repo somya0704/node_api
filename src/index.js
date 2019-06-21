4ae59654276a2fa244829abc3e569676b437c0b7
somya <somya.singhal0797@gmail.com>
2019-06-21 12:39:02 +0530

    express instance init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+let express = require('express')
+let app = express()
+ 
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.info(`Service has started on ${PORT}`))
