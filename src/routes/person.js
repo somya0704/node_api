04d6dabd5dce0b8d83842c2f1f50f67a5371aa05
somya <somya.singhal0797@gmail.com>
2019-06-21 17:39:59 +0530

    param property request object

--- src/routes/person.js
+++ src/routes/person.js
@@ -5,7 +5,10 @@ let router = express.Router()
 router.get('/person', (req,res) => {
     res.send('You have requested a person')
 })
-
+//Param property on the request obj
+router.get('/person/:name', (req,res) => {
+    res.send(`You have requested a person ${req.params.name}`)
+})
 
 
 module.exports = router
